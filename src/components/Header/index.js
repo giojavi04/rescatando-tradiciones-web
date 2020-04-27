6f645a78b1c1bfb9482e6137e9957487da9aaa45
Javier Montalvo <giojavi04@gmail.com>
2020-04-26 22:12:32 -0500

    fixed components tailwind

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,16 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Image from '../Image'
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <div>
+  <header className="fixed w-full z-30 top-0 text-white">
+    <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
       <h1 className="font-body">
         <Link
           to="/"
         >
+          <Image nameImage="logo-rescatando-tradiciones.png" alt="Rescatando tradiciones" className="w-1" />
           {siteTitle}
         </Link>
       </h1>
--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 import PropTypes from 'prop-types'
 import Img from "gatsby-image"
 
@@ -14,42 +14,35 @@ import Img from "gatsby-image"
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-const Image = ({ nameImage, alt }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        images: allFile {
-          edges {
-            node {
-              relativePath
-              name
-              childImageSharp {
-                fluid {
-                  ...GatsbyImageSharpFluid
-                }
+const Image = ({ nameImage, alt }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      images: allFile {
+        edges {
+          node {
+            relativePath
+            name
+            childImageSharp {
+              fluid(maxWidth: 1920) {
+                ...GatsbyImageSharpFluid
               }
             }
           }
         }
       }
-    `}
-    render={data => {
-      const image = data.images.edges.find(n => {
-        return n.node.relativePath.includes(nameImage);
-      });
-      if (!image) {
-        return null;
-      }
+    }
+  `)
 
-      //const imageSizes = image.node.childImageSharp.sizes; sizes={imageSizes}
-      return <Img alt={alt} fluid={image.node.childImageSharp.fluid} />;
-    }}
-  />
-)
+  const image = data.images.edges.find(n => {
+    return n.node.relativePath.includes(nameImage)
+  })
+
+  return <Img fluid={image.node.childImageSharp.fluid} alt={alt} />
+}
 
 Image.propTypes = {
   nameImage: PropTypes.string,
-  alt: PropTypes.string
+  atl: PropTypes.string
 }
 
 export default Image
