5e63307758e41e2831969ffc8718e59708058d93
Javier Montalvo <giojavi04@gmail.com>
2020-04-27 21:03:05 -0500

    implement vars to ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     fontFamily: {
       'sans': ['Merriweather', 'Sans-serif']
     },
-    extend: {},
+    extend: {
+      colors: {
+        "rt-background": "var(--rt-background)",
+        "rt-primary": "var(--rt-primary)",
+        "rt-secondary": "var(--rt-secondary)",
+        "rt-grey": "var(--rt-grey)",
+        "rt-grey-dark": "var(--grey-dark)",
+      },
+    },
   },
   variants: {},
   plugins: [],
