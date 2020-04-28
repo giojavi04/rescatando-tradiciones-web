7cc116861071b0ed5f5b5e9d88c49695b0a9fa35
Javier Montalvo <giojavi04@gmail.com>
2020-04-28 01:07:26 -0500

    finish maqueted header mobile

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -20,24 +20,40 @@ const Header = () => (
         </button>
       </div>
 
-      <div className="w-full bg-gray-900 lg:bg-transparent lg:flex lg:items-center lg:w-auto z-20" id="nav-content">
-        <ul className="text-white list-reset lg:flex justify-end flex-1 items-center">
-          <li className="lg:mr-3">
-            <a className="inline-block w-full py-6 px-4" href="#">Nosotros</a>
-          </li>
-          <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
-          <li className="lg:mr-3">
-            <a className="inline-block w-full py-6 px-4" href="#">Grupos</a>
-          </li>
-          <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
-          <li className="lg:mr-3">
-            <a className="inline-block w-full py-6 px-4" href="#">Eventos</a>
-          </li>
-          <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
-          <li className="lg:mr-3">
-            <a className="inline-block w-full py-6 px-4" href="#">Contacto</a>
-          </li>
-        </ul>
+      <div className="w-full lg:flex lg:items-center px-2">
+        <nav className="bg-gray-900 hidden lg:bg-transparent lg:flex lg:items-center lg:w-auto z-20" id="nav-content">
+          <ul className="text-white list-reset lg:flex justify-end flex-1 items-center">
+            <li className="lg:mr-3">
+              <a className="inline-block w-full py-6 px-4" href="#">Nosotros</a>
+            </li>
+            <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
+            <li className="lg:mr-3">
+              <a className="inline-block w-full py-6 px-4" href="#">Grupos</a>
+            </li>
+            <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
+            <li className="lg:mr-3">
+              <a className="inline-block w-full py-6 px-4" href="#">Eventos</a>
+            </li>
+            <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
+            <li className="lg:mr-3">
+              <a className="inline-block w-full py-6 px-4" href="#">Contacto</a>
+            </li>
+          </ul>
+        </nav>
+        <div>
+          <div className="flex items-center">
+            <h5 className="flex-initial mr-2 italic font-light text-xl md:text-2xl">Colectivo</h5>
+            <hr className="flex-auto border-b border-rt-primary my-0 py-0" />
+          </div>
+          <div className="text-center mb-4 mt-1">
+            <h2 className="text-rt-primary font-black text-3xl md:text-4xl">Rescatando Tradiciones</h2>
+          </div>
+          <hr className="border-rt-primary my-0 py-0" />
+          <div className="text-right my-4">
+            <h4 className="text-rt-secondary italic font-light text-2xl md:text-3xl">Confraternidad y Tradición</h4>
+          </div>
+          <hr className="border-rt-primary my-0 py-0" />
+        </div>
       </div>
     </div>
   </header>
