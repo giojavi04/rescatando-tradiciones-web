68ee0a0a1422d3e168d276737a0212f9c33cfabd
Javier Montalvo <giojavi04@gmail.com>
2020-04-28 19:03:45 -0500

    finish styled header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import Image from '../Image'
 
 const Header = () => (
   <header className="w-full z-30 top-0">
-    <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0">
+    <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 lg:pt-10">
       <div className="pl-4 flex items-center my-2">
         <Link
           className="w-10 lg:w-40 lg:mr-6"
@@ -20,7 +20,7 @@ const Header = () => (
         </button>
       </div>
 
-      <div className="w-full flex-col lg:w-auto lg:flex lg:flex-auto lg:items-center px-2">
+      <div className="w-full flex-col lg:w-auto lg:flex lg:flex-auto lg:items-center px-2 py-4 lg:py-0">
         <nav className="w-full bg-gray-900 hidden z-20 lg:bg-transparent lg:flex lg:items-center lg:-mb-5" id="nav-content">
           <ul className="text-white lg:text-rt-secondary list-reset lg:flex justify-end flex-1 items-center">
             <li>
@@ -50,7 +50,7 @@ const Header = () => (
           </div>
           <hr className="border-rt-primary my-0 py-0" />
           <div className="text-right my-4">
-            <h4 className="text-rt-secondary italic font-light text-2xl md:text-3xl">Confraternidad y Tradición</h4>
+            <h4 className="text-rt-secondary italic font-light text-2xl md:text-3xl pr-2">Confraternidad y Tradición</h4>
           </div>
           <hr className="border-rt-primary my-0 py-0" />
         </div>
