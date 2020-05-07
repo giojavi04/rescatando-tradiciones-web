6eaf56244f68aa49aa01879dbb34b368184f0006
Javier Montalvo <giojavi04@gmail.com>
2020-05-06 19:31:56 -0500

    minnor change

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import Image from '../Image'
 
 const Footer = () => (
   <footer className="footerRT border-b py-8">
-    <div className="container mx-auto flex flex-wrap pt-4 pb-12">
+    <div className="container mx-auto flex flex-wrap items-baseline pt-4 pb-12">
       <div className="w-full md:w-1/4 p-6 flex flex-col flex-grow flex-shrink">
         <Link
           className="w-10 lg:w-40 lg:mr-6"
