816227e994a66177aed79a70cdf4926592b4148e
Javier <jmontalvo@grupocentrico.com>
2020-06-22 21:59:15 -0500

    background in footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,6 +2,9 @@ import styled from "styled-components"
 import tw from "twin.macro"
 
 export const Footer = styled.footer`
-  background-image: url("./images/footer-bg-rescatando-tradiciones.png");
-  ${tw`border-b py-8`};
+  background-image: url("https://raw.githubusercontent.com/giojavi04/rescatando-tradiciones-web/develop/src/images/footer-bg-rescatando-tradiciones.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center left;
+  ${tw`border-b pb-48 pt-64`};
 `
