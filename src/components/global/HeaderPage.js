0f21475794cf0d6e50c094d3cee607b5873b4484
Javier Montalvo <giojavi04@gmail.com>
2021-04-25 16:39:52 -0500

    contact page

--- src/components/global/HeaderPage.js
+++ src/components/global/HeaderPage.js
@@ -10,7 +10,7 @@ const HeaderPage = ({ imgSrc, title, description }) => {
           src={imgSrc}
           alt=""
         />
-        <div className="absolute inset-0 bg-gray-400" style={{ mixBlendMode: 'multiply' }} aria-hidden="true" />
+        <div className="absolute inset-0 bg-gray-500" style={{ mixBlendMode: 'multiply' }} aria-hidden="true" />
       </div>
       <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
         <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">{title}</h1>
--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -1,10 +1,24 @@
 import * as React from "react"
 import { Helmet } from "react-helmet"
 
+import HeaderPage from '../components/global/HeaderPage'
+import Content from '../components/contact/content'
+import Info from '../components/contact/info'
+
+import contactImg from '../images/contacto/contact-header.png'
+
 // markup
 const ContactPage = () => {
   return (
-    <div>Contacto</div>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Rescatando Tradiciones | Contacto</title>
+      </Helmet>
+      <HeaderPage imgSrc={contactImg} title="Contacto" description="Si deseas conocer más sobre nosotros, llena el formulario." />
+      <Content />
+      <Info />
+    </>
   )
 }
 
