c37579e2b5839226f3b9c7d4de3cf580947eb05f
Javier Montalvo <giojavi04@gmail.com>
2021-04-25 16:08:08 -0500

    improvements in header page

--- src/components/global/HeaderPage.js
+++ src/components/global/HeaderPage.js
@@ -3,18 +3,18 @@ import * as React from "react"
 // markup
 const HeaderPage = ({ imgSrc, title, description }) => {
   return (
-    <div className="relative bg-rtPrimary">
+    <div className="relative bg-rtSecondary">
       <div className="absolute inset-0">
         <img
           className="w-full h-full object-cover"
           src={imgSrc}
           alt=""
         />
-        <div className="absolute inset-0 bg-rtPrimary" style={{ mixBlendMode: 'multiply' }} aria-hidden="true" />
+        <div className="absolute inset-0 bg-gray-400" style={{ mixBlendMode: 'multiply' }} aria-hidden="true" />
       </div>
       <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
         <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">{title}</h1>
-        <p className="mt-6 text-xl text-indigo-100 max-w-3xl">
+        <p className="mt-6 text-xl text-gray-50 max-w-3xl">
           {description}
         </p>
       </div>
