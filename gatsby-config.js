21300c66b3ed1fd0971772e0be9c8fadabb7ac6f
Javier Montalvo <giojavi04@gmail.com>
2021-05-20 20:30:00 -0500

    implement gta

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: "test-----id-google",
+        trackingId: "G-WZWGYMZDJJ",
       },
     },
     "gatsby-plugin-react-helmet",
