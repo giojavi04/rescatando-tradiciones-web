3f05c0e3173807cd959f80138cc0b0d564ee58d3
Javier Montalvo <giojavi04@gmail.com>
2021-05-27 22:13:54 -0500

    improvements

--- src/components/global/EventPost.js
+++ src/components/global/EventPost.js
@@ -52,7 +52,7 @@ const EventPost = ({
         <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0">
           {/* Content area */}
           <div className="pt-12 sm:pt-16 lg:pt-20">
-            <h2 className="text-3xl text-gray-900 font-extrabold tracking-tight sm:text-4xl">
+            <h2 className="text-3xl text-gray-900 font-extrabold tracking-tight sm:text-4xl mb-6">
               {title}
             </h2>
             <ContentHTML content={content} />
