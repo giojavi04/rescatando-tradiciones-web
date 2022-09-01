8d7bdb87f9138665ea74d82acc4dcbcfe6c0dbd4
Javier Montalvo <giojavi04@gmail.com>
2022-09-01 11:49:45 -0500

    mod: styles general

--- src/components/events/content.js
+++ src/components/events/content.js
@@ -1,8 +1,8 @@
-import * as React from "react"
-import { Link } from 'gatsby'
-import { GatsbyImage } from 'gatsby-plugin-image'
+import * as React from "react";
+import { Link } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
 
-import ContentHTML from '../global/ContentHTML'
+import ContentHTML from "../global/ContentHTML";
 
 // markup
 const Content = ({ data, events }) => {
@@ -19,19 +19,27 @@ const Content = ({ data, events }) => {
                   className="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-rtSecondary hover:bg-gray-900"
                 >
                   Contáctanos
-              </Link>
+                </Link>
               </div>
             </div>
           </div>
-          <div className="mt-8 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
-            {events.map(event => (
-              <Link key={event.node.frontmatter.title} to={event.node.fields.slug}>
-                <div className="col-span-1 flex flex-col justify-center items-center py-8 px-8 bg-gray-100">
-                  <h3 className="text-lg font-medium text-gray-900 tracking-tight hover:text-rtSecondary">{event.node.frontmatter.title}</h3>
+          <div className="mt-8 grid grid-cols-1 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
+            {events.map((event) => (
+              <Link
+                key={event.node.frontmatter.title}
+                to={event.node.fields.slug}
+              >
+                <div className="col-span-1 flex flex-col justify-center items-stretch h-60 min-h-fit py-8 px-8 bg-gray-100">
+                  <h3 className="text-lg font-medium text-gray-900 tracking-tight hover:text-rtSecondary">
+                    {event.node.frontmatter.title}
+                  </h3>
                   <GatsbyImage
                     className="max-h-48"
                     imgClassName="filter grayscale hover:grayscale-0"
-                    image={event.node.frontmatter.eventLogoEvent.childImageSharp.gatsbyImageData}
+                    image={
+                      event.node.frontmatter.eventLogoEvent.childImageSharp
+                        .gatsbyImageData
+                    }
                     alt={event.node.frontmatter.title}
                   />
                 </div>
@@ -41,7 +49,7 @@ const Content = ({ data, events }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
--- src/components/global/HeaderPage.js
+++ src/components/global/HeaderPage.js
@@ -1,6 +1,6 @@
-import * as React from "react"
-import { GatsbyImage } from 'gatsby-plugin-image'
-import { Transition } from '@headlessui/react'
+import * as React from "react";
+import { GatsbyImage } from "gatsby-plugin-image";
+import { Transition } from "@headlessui/react";
 
 // markup
 const HeaderPage = ({ image, title, description }) => {
@@ -12,13 +12,14 @@ const HeaderPage = ({ image, title, description }) => {
           image={image}
           alt={title}
         />
-        <div className="absolute inset-0 bg-gray-500" style={{ mixBlendMode: 'multiply' }} aria-hidden="true" />
+        <div
+          className="absolute inset-0 bg-gray-500"
+          style={{ mixBlendMode: "multiply" }}
+          aria-hidden="true"
+        />
       </div>
-      <Transition
-        appear={true}
-        show={true}
-      >
-        <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
+      <Transition appear={true} show={true}>
+        <div className="relative max-w-7xl mx-auto py-20 px-4 sm:py-32 sm:px-6 lg:px-8">
           <Transition.Child
             enter="transition-opacity ease-linear duration-500"
             enterFrom="opacity-0"
@@ -27,7 +28,9 @@ const HeaderPage = ({ image, title, description }) => {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">{title}</h1>
+            <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">
+              {title}
+            </h1>
           </Transition.Child>
           <Transition.Child
             enter="transition ease-linear duration-800 transform"
@@ -37,14 +40,12 @@ const HeaderPage = ({ image, title, description }) => {
             leaveFrom="translate-y-0"
             leaveTo="translate-y-8"
           >
-            <p className="mt-6 text-xl text-gray-50 max-w-3xl">
-              {description}
-            </p>
+            <p className="mt-6 text-xl text-gray-50 max-w-3xl">{description}</p>
           </Transition.Child>
         </div>
       </Transition>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderPage
+export default HeaderPage;
